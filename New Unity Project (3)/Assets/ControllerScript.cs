a14cd3bfb31e8477db40d048c761335fe92c4331
binbinbin13 <142992693+binbinbin13@users.noreply.github.com>
2024-01-22 13:28:57 +0800

    Updated

--- New Unity Project (3)/Assets/ControllerScript.cs
+++ New Unity Project (3)/Assets/ControllerScript.cs
@@ -7,6 +7,10 @@ public class ControllerScript : MonoBehaviour
 {
     public Toggle USD;
     public Toggle YEN;
+    public Toggle RM;
+    public Toggle EUR;
+    public Toggle KRW;
+    public Toggle TWD;
     private float SGDamount;
     public InputField TxtAmount;
     private float Value;
@@ -44,7 +48,27 @@ public class ControllerScript : MonoBehaviour
                 Value = SGDamount * 82.78f;
                 ValueTxt.text = Value.ToString() + " JPY";
             }
-            
+            if (RM.GetComponent<Toggle>().isOn == true)
+            {
+                Value = SGDamount * 3.08f;
+                ValueTxt.text = Value.ToString() + " JPY";
+            }
+            if (EUR.GetComponent<Toggle>().isOn == true)
+            {
+                Value = SGDamount * 0.63f;
+                ValueTxt.text = Value.ToString() + " JPY";
+            }
+            if (KRW.GetComponent<Toggle>().isOn == true)
+            {
+                Value = SGDamount * 881.54f;
+                ValueTxt.text = Value.ToString() + " JPY";
+            }
+            if (TWD.GetComponent<Toggle>().isOn == true)
+            {
+                Value = SGDamount * 20.73f;
+                ValueTxt.text = Value.ToString() + " JPY";
+            }
+
 
         }
         catch(System.Exception)
@@ -59,6 +83,10 @@ public class ControllerScript : MonoBehaviour
     {
         YEN.GetComponent<Toggle>().isOn = false;
         USD.GetComponent<Toggle>().isOn = false;
+        RM.GetComponent<Toggle>().isOn = false;
+        EUR.GetComponent<Toggle>().isOn = false;
+        KRW.GetComponent<Toggle>().isOn = false;
+        TWD.GetComponent<Toggle>().isOn = false;
         ValueTxt.text = "";
         TxtAmount.text = "";
     }
